b2f49b6e7b867975858132687f758fed2ed3fe3f
Shihas R <mail4shihasr@gmail.com>
2013-04-18 14:53:13 +0200

    Initial Draft

---
+++ Example_git_sh1/sha_1.cpp
@@ -0,0 +1,27 @@
+/*
+ * sha_1.cpp
+ *
+ *  Created on: Apr 17, 2013
+ *      Author: sradhakr
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> vnum;
+	int num;
+	char uchoice;
+
+	cout << "Please enter a number" << endl;
+	cin >> num;
+
+}
+
+
+
+
+
